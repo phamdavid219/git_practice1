d9d0f24f6b16cdd1303195ff1229c84f84416a93
CUONG PHAM <phamdavid219@gmail.com>
2022-01-11 14:03:21 -0500

    added a folder called power into the list

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -4,5 +4,7 @@ public class HelloWorld {
 //doing work 3
     //awesome work3
     //awesome work4
-
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
 }
